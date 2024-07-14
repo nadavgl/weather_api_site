9cb9069ccc2b0eaa6a7c50a0189ac08414cc2c6a
nadavgl <ngliksht@gmail.com>
2024-07-14 18:37:05 -0400

    made the currentweather icon dynamic

--- index.js
+++ index.js
@@ -19,91 +19,101 @@ function getRecentSearches() {
 
 
 
-function currentWeatherData(lat, lon){
-    let URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-    let iconURL = `https://openweathermap.org/img/wn/10d@2x.png`
-    fetch(URL)
+function currentWeatherData(lat, lon) {
+  let URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+  fetch(URL)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        
-        document.getElementById('current').innerHTML = '';
+      console.log(data);
 
-        let h1 = document.createElement("H1");
-        h1.textContent = `${data.name} (${dayjs().format('MM/DD/YYYY')})`;
-        document.getElementById("current").append(h1);
+      document.getElementById('current').innerHTML = '';
+      let iconCode = data.weather[0].icon;
+      let iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
-        let temp = document.createElement("p");
-        temp.textContent = `Temp: ${data.main.temp} F`;
-        temp.setAttribute("class", "currentWeather");
-        document.getElementById("current").append(temp);
+      let icon = document.createElement("img");
+      icon.setAttribute("src", iconURL);
+      document.getElementById("current").append(icon);
 
-        let wind = document.createElement("p");
-        wind.textContent = `Wind: ${data.wind.speed} MPH`;
-        wind.setAttribute("class", "currentWeather");
-        document.getElementById("current").append(wind);
+      let h1 = document.createElement("H1");
+      h1.textContent = `${data.name} (${dayjs().format('MM/DD/YYYY')})`;
+      document.getElementById("current").append(h1);
 
-        let humidity = document.createElement("p");
-        humidity.textContent = `Humidity: ${data.main.humidity} %`;
-        humidity.setAttribute("class", "currentWeather");
-        document.getElementById("current").append(humidity);
+      let temp = document.createElement("p");
+      temp.textContent = `Temp: ${data.main.temp} F`;
+      temp.setAttribute("class", "currentWeather");
+      document.getElementById("current").append(temp);
+
+      let wind = document.createElement("p");
+      wind.textContent = `Wind: ${data.wind.speed} MPH`;
+      wind.setAttribute("class", "currentWeather");
+      document.getElementById("current").append(wind);
+
+      let humidity = document.createElement("p");
+      humidity.textContent = `Humidity: ${data.main.humidity} %`;
+      humidity.setAttribute("class", "currentWeather");
+      document.getElementById("current").append(humidity);
 
     })
 }
 
 function getFiveDayForecast(lat, lon) {
-    let URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    fetch(URL)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data); // This will contain the 5-day forecast data
+  let URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data); // This will contain the 5-day forecast data
 
-        // Filter the forecast data for 12 PM each day
-        const filteredForecast = data.list.filter(forecast => {
-            // Check if the forecast time is 12:00:00
-            return new Date(forecast.dt * 1000).getUTCHours() === 12;
-        });
+      // Filter the forecast data for 12 PM each day
+      const filteredForecast = data.list.filter(forecast => {
+        // Check if the forecast time is 12:00:00
+        return new Date(forecast.dt * 1000).getUTCHours() === 12;
+      });
 
-        document.getElementById('forecast').innerHTML = '';
+      document.getElementById('forecast').innerHTML = '';
 
 
-        // Display the filtered forecast data on the webpage
-        filteredForecast.forEach(forecast => {
-            
-            let h1 = document.createElement("H1");
-            const date = dayjs(forecast.dt_txt).format('MM/DD/YYYY');
-            h1.textContent = `${data.city.name} (${date})`;
-            document.getElementById("forecast").append(h1);
+      // Display the filtered forecast data on the webpage
+      filteredForecast.forEach(forecast => {
 
-            let temp = document.createElement("p");
-            temp.textContent = `Temp: ${forecast.main.temp} F`;
-            temp.setAttribute("class", "forecastWeather");
-            document.getElementById("forecast").append(temp);
 
-            let wind = document.createElement("p");
-            wind.textContent = `Wind: ${forecast.wind.speed} MPH`;
-            wind.setAttribute("class", "forecastWeather");
-            document.getElementById("forecast").append(wind);
 
-            let humidity = document.createElement("p");
-            humidity.textContent = `Humidity: ${forecast.main.humidity} %`;
-            humidity.setAttribute("class", "forecastWeather");
-            document.getElementById("forecast").append(humidity);
-        });
-      })
+        let h1 = document.createElement("H1");
+        h1.setAttribute("class", "card-title")
+        const date = dayjs(forecast.dt_txt).format('MM/DD/YYYY');
+        h1.textContent = `${data.city.name} (${date})`;
+        document.getElementById("forecast").append(h1);
+
+
+        let temp = document.createElement("p");
+        temp.textContent = `Temp: ${forecast.main.temp} F`;
+        temp.setAttribute("class", "forecastWeather");
+        document.getElementById("forecast").append(temp);
+
+        let wind = document.createElement("p");
+        wind.textContent = `Wind: ${forecast.wind.speed} MPH`;
+        wind.setAttribute("class", "forecastWeather");
+        document.getElementById("forecast").append(wind);
+
+        let humidity = document.createElement("p");
+        humidity.textContent = `Humidity: ${forecast.main.humidity} %`;
+        humidity.setAttribute("class", "forecastWeather");
+        document.getElementById("forecast").append(humidity);
+      });
+    })
 }
 
 
 
-document.getElementById('button').addEventListener("click", function(e) {
-e.preventDefault();
-cityName = document.getElementById('cityname').value;
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`)
+document.getElementById('button').addEventListener("click", function (e) {
+  e.preventDefault();
+  cityName = document.getElementById('cityname').value;
+  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`)
     .then(res => res.json())
     .then(data => {
-        //console.log(data);
-        currentWeatherData(data[0].lat, data[0].lon);
-        getFiveDayForecast(data[0].lat, data[0].lon);
+      //console.log(data);
+      currentWeatherData(data[0].lat, data[0].lon);
+      getFiveDayForecast(data[0].lat, data[0].lon);
 
     })
 });
